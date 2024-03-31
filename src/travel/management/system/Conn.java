bebb8c19b305b91986e1ecf78d0c2fafb80c83e2
shubhamp13 <shubhampuri8297@gmail.com>
2024-03-31 18:12:48 +0530

    first

---
+++ src/travel/management/system/Conn.java
@@ -0,0 +1,15 @@
+package travel.management.system;
+
+import java.sql.*;  
+
+public class Conn{
+   Connection c;
+   Statement s;
+   Conn(){
+       try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+           c = DriverManager.getConnection("jdbc:mysql://localhost:3310/ttms", "root", "root");
+           s = c.createStatement();
+         }catch(Exception e){}  
+    }  
+}
